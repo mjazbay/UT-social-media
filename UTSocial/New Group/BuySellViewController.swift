7f44e63e377ec3075b81c4b03ab6a974ae9bb2b5
Maksat Zhazbayev <longhorn18@Maksats-MacBook-Pro.local>
2019-04-15 00:07:08 -0500

    Progress 3

---
+++ UTSocial/New Group/BuySellViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BuySellViewController.swift
+//  UTSocial
+//
+//  Created by Maksat Zhazbayev on 4/12/19.
+//  Copyright © 2019 Maksat Zhazbayev. All rights reserved.
+//
+
+import UIKit
+
+class BuySellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
